7f6eabb9a47d0bb69dc3d75779be130d8ce6b53a
Henni <henni.wie@gmail.com>
2024-05-30 15:35:33 +0200

    noch nicht funktioniertend

--- incl/philo.h
+++ incl/philo.h
@@ -6,7 +6,7 @@
 /*   By: hwiemann <hwiemann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 11:28:02 by hwiemann          #+#    #+#             */
-/*   Updated: 2024/05/30 11:12:45 by hwiemann         ###   ########.fr       */
+/*   Updated: 2024/05/30 15:01:42 by hwiemann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,23 @@ typedef struct {
 	int				philo_num;
 	int				time_to_eat;
 	int				time_to_think;
+	int				time_to_sleep;
+	int				time_to_die;
+	long			last_meal_time;
 	int 			id;
 	pthread_mutex_t	*forks;
 } philo_args;
 
 
-void	*ft_philo(philo_args *args);
-int		create_philos(pthread_t	*philosoph, philo_args *args);
-int		wait_for_philos(pthread_t *philosoph, philo_args *args);
-void	eat(int philosopher, philo_args *args);
-void	think(int philosopher, philo_args *args);
-void	fork_mutex_init(pthread_mutex_t *fork, philo_args *args);
-void	destroy_forks(pthread_mutex_t *fork, philo_args *args);
+void		*ft_philo(void *arg);
+long long	the_time();
+int			create_philos(pthread_t	*philosoph, philo_args *args);
+int			wait_for_philos(pthread_t *philosoph, philo_args *args);
+void		eat(int philosopher, philo_args *args);
+void		think(int philosopher, philo_args *args);
+void		sleepy(int philosopher, philo_args *args);
+void		fork_mutex_init(pthread_mutex_t *fork, philo_args *args);
+void		destroy_forks(pthread_mutex_t *fork, philo_args *args);
 philo_args	*init_philo_args(pthread_mutex_t *forks, char **argv);
 
 
--- src/philo.c
+++ src/philo.c
@@ -6,19 +6,19 @@
 /*   By: hwiemann <hwiemann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 11:27:37 by hwiemann          #+#    #+#             */
-/*   Updated: 2024/05/30 11:13:06 by hwiemann         ###   ########.fr       */
+/*   Updated: 2024/05/30 15:32:38 by hwiemann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/philo.h"
 
-void	*ft_philo(philo_args *args)  //muss eigentlich void *arg sein
+void	*ft_philo (void *arg)    //(philo_args *args)  //muss eigentlich void *arg sein
 {
 	int	id;
-//	philo_args	*args;
+	philo_args	*args;
 	pthread_mutex_t *fork;
 
-//	args = (philo_args *)arg;
+	args = (philo_args *)arg;
 	id = args->id;
 	fork = args->forks;
 
@@ -38,8 +38,22 @@ void	*ft_philo(philo_args *args)  //muss eigentlich void *arg sein
 		eat(id, args);
 		pthread_mutex_unlock(&fork[id]);
 		pthread_mutex_unlock(&fork[(id + 1) % args->philo_num]);
+		sleepy(id, args);
+		if((the_time() - args->last_meal_time) > args->time_to_die)
+		{
+			printf("Philo %d died\n", id);
+			break ;
+		}
 	}
 	return (NULL);
+}
+
+long long	the_time()
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
 
 void	think(int philosopher, philo_args *args)
@@ -51,9 +65,15 @@ void	think(int philosopher, philo_args *args)
 void	eat(int philosopher, philo_args *args)
 {
 	printf("Philo %d is eating \n", philosopher);
+	args->last_meal_time = the_time();
 	usleep((rand() % 100) * args->time_to_eat);
 }
 
+void	sleepy(int philosopher, philo_args *args)
+{
+	printf("Philo %d is sleeping\n", philosopher);
+	usleep((rand() % 100) * args->time_to_sleep);
+}
 
 int	create_philos(pthread_t	*philosoph, philo_args *args)
 {
@@ -63,7 +83,7 @@ int	create_philos(pthread_t	*philosoph, philo_args *args)
 	while (i < args->philo_num)
 	{
 		args[i].id = i;
-		if(pthread_create(&philosoph[i], NULL, ft_philo(args), &args[i]) != 0)
+		if(pthread_create(&philosoph[i], NULL, ft_philo, &args[i]) != 0)
 		{
 			printf("Error in creating threads\n");
 			return (1);
@@ -120,38 +140,40 @@ philo_args	*init_philo_args(pthread_mutex_t *forks, char **argv)
 	philo_args	*args;
 
 	i = 0;
-	args = malloc(sizeof(philo_args) * NUM_PHILO);
+	args = malloc(sizeof(philo_args) * (atoi(argv[1])));
 	if (!args)
 	{
 		printf("malloc error \n");
 		return (NULL);
 	}
-	while (i < NUM_PHILO)
+	while (i < atoi(argv[1]))
 	{
 		args[i].forks = forks;
 		i++;
 	}
-	if (argv[1] && argv[2] && argv[3])
+	if (argv[2] && argv[3])
 	{
 		args->philo_num = atoi(argv[1]);
 		args->time_to_eat = atoi(argv[2]) * 1000;
 		args->time_to_think = atoi(argv[3]) * 1000;
+		args->time_to_sleep = atoi(argv[4]) * 1000;
+		args->time_to_die = atoi(argv[5]) * 1000;
 	}
 	return (args);
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_mutex_t	forks[NUM_PHILO];
-	pthread_t		philosophers[NUM_PHILO];
-	philo_args		*args;
-
-	if (argc == 4)
+	if (argc == 6)
 	{
+		pthread_mutex_t	forks[atoi(argv[1])];
+		pthread_t		philosophers[atoi(argv[1])];
+		philo_args		*args;
+
 		args = init_philo_args(forks, argv);
-		fork_mutex_init(forks, args);
 		if(!args)
 			return (1);
+		fork_mutex_init(forks, args);
 		if (create_philos(philosophers, args) != 0)
 		{
 			free(args);
@@ -162,17 +184,10 @@ int	main(int argc, char **argv)
 			free(args);
 			return (1);
 		}
-		while (1)
-		{
-			usleep(1000);
-		}
 		destroy_forks(forks, args);
 		free(args);
 	}
 	else
-	{
-		printf("Error Main %s\n", argv[0]);
-		return (1);
-	}
+		printf("Error with Arguments order should be num_of_philo time_to_eat time_to_think time_to_die\n");
 	return (0);
 }
